b69d36f890942567865f05c26e15762319ddceb5
Ben Bullock <benkasminbullock@gmail.com>
2018-04-03 07:51:29 +0900

    Add some test and build scripts, clean up makefile

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ BIN=/home/ben/bin/makereadme
 install:
 	rm -f $(BIN)
 	ln makereadme $(BIN)
+
+test:
+	prove ./test.pl
---
+++ test.pl
@@ -0,0 +1,17 @@
+#!/home/ben/software/install/bin/perl
+use warnings;
+use strict;
+use utf8;
+use FindBin '$Bin';
+use Test::More;
+use IPC::Run3;
+my $x = "$Bin/makereadme";
+ok (-f $x, "File exists");
+chdir ("$Bin/t/silly-distro") or die $!;
+run3 ("$x", undef, \my $output, \my $errors);
+if ($errors) {
+note ($errors);
+}
+ok (! $errors, "no errors");
+ok ($output, "got output");
+done_testing ();
